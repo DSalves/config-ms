acfdffe95bf5f1761c600c61b703427148e508b6
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-25 10:12:38 -0300

    --

--- src/main/java/br/com/ifc/config/main/Application.java
+++ src/main/java/br/com/ifc/config/main/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= {"br.com.ifc.config.security"})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
